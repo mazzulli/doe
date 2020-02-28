8d875e3a5fa67139599988fd3822fd5af066f43a
mazzulli <mazzulli@live.com>
2020-02-27 23:10:08 -0300

    Primeira carga

---
+++ readme.txt
@@ -0,0 +1,9 @@
+site: doemazz.000webhostapp.com
+senha: doe123456
+bd: doe
+user: postgres
+pass: 123456
+
+umbler
+mazzulli@live.com
+info1978
---
+++ server.js
@@ -0,0 +1,71 @@
+// configurando o servidor
+const express = require("express")
+const server = express()
+
+// configurar o servidor para apresentar arquivos estáticos
+server.use(express.static("public"))
+
+// habilitar o body do formulário
+server.use(express.urlencoded({extended: true}))
+
+// conexão com o bando de dados
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: '123456',
+    host: 'localhost',
+    port: 5432,
+    database: "doe"
+})
+
+
+// configurando a template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server,
+    noCache: true,
+})
+
+// configurando a apresentação da pagina
+server.get("/", function(req, res){
+    db.query("SELECT * FROM donors", function(err, result){
+        if (err) return res.send("Erro ao tentar ler os dados no banco.")
+
+        const donors = result.rows
+        return res.render("index.html", { donors })
+    })
+    
+    
+})
+
+// pegar dados do formulário
+server.post("/", function(req, res){
+    const name = req.body.name;
+    const email = req.body.email; 
+    const blood = req.body.blood; 
+
+    if (name == "" || email == "" || blood == "" )
+        return res.send("Todos os campos são obrigatórios!")
+    
+    // adicionar valores no array
+    const query = `INSERT INTO donors ("name", "email", "blood") VALUES ($1,$2,$3)`
+    const values = [name, email, blood]
+
+    db.query(query, values, function(err){
+        
+        // fluxo de erro
+        if (err) return res.send("Erro ao inserir os dados no banco.")
+        
+        // fluxo correto
+        return res.redirect("/")
+    })
+
+    
+})
+
+// ligar o servidor
+server.listen(3000, function(){
+    console.log("Servidor iniciado.")
+})
+
+
