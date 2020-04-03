f670c50a37da5d8db63d6728553df477de5ef78a
mazzulli <mazzulli@live.com>
2020-04-02 23:01:53 -0300

    Carga inicial

--- server.js
+++ server.js
@@ -8,15 +8,25 @@ server.use(express.static("public"))
 // habilitar o body do formulário
 server.use(express.urlencoded({extended: true}))
 
-// conexão com o bando de dados
-const Pool = require('pg').Pool
-const db = new Pool({
-    user: 'postgres',
-    password: '123456',
-    host: 'localhost',
-    port: 5432,
-    database: "doe"
-})
+// conexão com o bando de dados POSTGRES
+// const Pool = require('pg').Pool
+// const db = new Pool({
+//     user: 'postgres',
+//     password: '123456',
+//     host: 'localhost',
+//     port: 5432,
+//     database: "doe"
+// })
+
+// conexão com o banco de dados MYSQL
+const mysql = require('mysql');
+const db = mysql.createConnection({
+  host: "mysql669.umbler.com",
+  port: 41890,
+  user: "doeadmin",
+  password: "info1978",
+  database: "doe",
+});
 
 
 // configurando a template engine
@@ -28,9 +38,9 @@ nunjucks.configure("./", {
 
 // configurando a apresentação da pagina
 server.get("/", function(req, res){
-    db.query("SELECT * FROM donors", function(err, result){
-        if (err) return res.send("Erro ao tentar ler os dados no banco.")
-
+    db.query("SELECT * FROM doe.donors;", function(err, result){
+        if (err) return res.send(err) // "Erro ao tentar conectar no banco de dados."
+        console.log(result)
         const donors = result.rows
         return res.render("index.html", { donors })
     })
@@ -48,18 +58,21 @@ server.post("/", function(req, res){
         return res.send("Todos os campos são obrigatórios!")
     
     // adicionar valores no array
-    const query = `INSERT INTO donors ("name", "email", "blood") VALUES ($1,$2,$3)`
-    const values = [name, email, blood]
+    // const query = `INSERT INTO donors ("name", "email", "blood") VALUES ($1,$2,$3)`
+    // const values = [name, email, blood]
 
-    db.query(query, values, function(err){
+    const query = "INSERT INTO donors (name, email, blood) VALUES ('" + name + "','" + email + "','" + blood + "')"
+    
+    console.log(query)
+    
+    db.query(query, function(err){
         
-        // fluxo de erro
-        if (err) return res.send("Erro ao inserir os dados no banco.")
+        // fluxo de erro        
+        if (err) return res.send(err) //"Erro ao inserir os dados no banco."
         
         // fluxo correto
         return res.redirect("/")
     })
-
     
 })
 
